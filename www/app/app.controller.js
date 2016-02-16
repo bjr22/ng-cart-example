5692f80cd7f9c8006fb1397be87c01c3698fa454
Julian Torres Cardenas <julian.torres@worldline.net>
2016-02-16 09:53:05 +0100

    mocked system working

--- www/app/app.controller.js
+++ www/app/app.controller.js
@@ -2,8 +2,13 @@
   'use strict';
   angular
     .module('ngCartExample')
-    .controller('AppCtrl', function($scope, $http, ngCart) {
+    .controller('AppCtrl', function($scope, $http, ngCart, MockedData) {
       ngCart.setTaxRate(7.5);
-      ngCart.setShipping(2.99);    
+      ngCart.setShipping(2.99);
+    
+      var vm = $scope;
+      MockedData.getUsers().then(function (response) {
+        console.log("users", response);
+      });
   });
 })();
--- www/app/config/app.mocked.data.js
+++ www/app/config/app.mocked.data.js
@@ -7,19 +7,43 @@
   MockedData.$inject = ['$http', '$q']
   function MockedData ($http, $q) {
     var getSomeData = function (source) {
-      var defer = $q.defer();
+      var deferred = $q.defer();
       $http({
         method: 'GET',
         url: source
       }).then(function successCallback(response) {
-        
+        deferred.resolve(response.data);
       }, function errorCallback(response) {
-        
+        deferred.reject({err: response, msg: 'something went wrong'});
       });
-    }
+      return deferred.promise;
+    };
+    var path = 'json/';
+    var extension = '.json';
     return {
       getCategories: function () {
-        
+        return getSomeData(path + 'category' + extension);
+      },
+      getItems: function () {
+        return getSomeData(path + 'item' + extension);
+      },
+      getOpinions: function () {
+        return getSomeData(path + 'opinion' + extension);
+      },
+      getOrders: function () {
+        return getSomeData(path + 'order' + extension);
+      },
+      getOrderItems: function () {
+        return getSomeData(path + 'orderItem' + extension);
+      },
+      getSections: function () {
+        return getSomeData(path + 'section' + extension);
+      },
+      getShipmentAddresses: function () {
+        return getSomeData(path + 'shipmentAddress' + extension);
+      },
+      getUsers: function () {
+        return getSomeData(path + 'user' + extension);
       }
     }
   }
